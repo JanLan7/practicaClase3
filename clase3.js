c15bd502170479c30cbac31e7ec343a5d3dd7a11
JanLan <idiomasjanlan@gmail.com>
2024-10-27 21:06:04 -0300

    subiendo practica clase 3

---
+++ clase3.js
@@ -0,0 +1,43 @@
+// function hola(){
+//     console.log("hola mundo")
+// }
+
+
+// function sumar(a,b){
+//     return a + b;
+// }
+
+// let resultado = sumar(2,3);
+// console.log(resultado);
+
+
+
+// function calcularAreaTriangulo(base,altura){
+//     return(base*altura)/2;
+// }
+
+// function saludar(){
+//     //cuerpo de la funcion.
+//     console.log("Hola, desde mi función")
+// }
+// //invocar
+// saludar();
+
+
+
+// function saludar(nombre) {
+//     console.log("Hola "+ nombre);
+    
+    
+// }
+// saludar("Joel");
+
+
+
+
+function saludar(nombre, edad) {
+    console.log("Hola "+ nombre);
+    console.log("Tu edad es "+ edad);
+    
+}
+saludar("José", 40);
